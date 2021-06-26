a7a34f1dd777ce4f81de2080e1f5bb563d032dd7
Vaclav Bubnik <vbubnik@email.cz>
2021-06-26 14:21:17 +0200

    Updated.

--- modules/core/src/common/ColorPalette.cpp
+++ modules/core/src/common/ColorPalette.cpp
@@ -144,7 +144,85 @@ const std::vector<Vector3<float>> ColorPalette::RedYellow16 = {
     {1.0F, 0xf * (1.0F / 15), 0.0F},
 };
 
-const std::vector<Vector3<float>> ColorPalette::blueCyanGreenYellowRed16 = {
+const std::vector<Vector3<float>> ColorPalette::BlueYellow16 = {
+    {0x0 * (1.0F / 15), 1.0F, 1.0F - 0x0 * (1.0F / 15)},
+    {0x1 * (1.0F / 15), 1.0F, 1.0F - 0x1 * (1.0F / 15)},
+    {0x2 * (1.0F / 15), 1.0F, 1.0F - 0x2 * (1.0F / 15)},
+    {0x3 * (1.0F / 15), 1.0F, 1.0F - 0x3 * (1.0F / 15)},
+    {0x4 * (1.0F / 15), 1.0F, 1.0F - 0x4 * (1.0F / 15)},
+    {0x5 * (1.0F / 15), 1.0F, 1.0F - 0x5 * (1.0F / 15)},
+    {0x6 * (1.0F / 15), 1.0F, 1.0F - 0x6 * (1.0F / 15)},
+    {0x7 * (1.0F / 15), 1.0F, 1.0F - 0x7 * (1.0F / 15)},
+    {0x8 * (1.0F / 15), 1.0F, 1.0F - 0x8 * (1.0F / 15)},
+    {0x9 * (1.0F / 15), 1.0F, 1.0F - 0x9 * (1.0F / 15)},
+    {0xa * (1.0F / 15), 1.0F, 1.0F - 0xa * (1.0F / 15)},
+    {0xb * (1.0F / 15), 1.0F, 1.0F - 0xb * (1.0F / 15)},
+    {0xc * (1.0F / 15), 1.0F, 1.0F - 0xc * (1.0F / 15)},
+    {0xd * (1.0F / 15), 1.0F, 1.0F - 0xd * (1.0F / 15)},
+    {0xe * (1.0F / 15), 1.0F, 1.0F - 0xe * (1.0F / 15)},
+    {0xf * (1.0F / 15), 1.0F, 1.0F - 0xf * (1.0F / 15)},
+};
+
+const std::vector<Vector3<float>> ColorPalette::BlueCyanYellowRed16 = {
+    {0.000000F, 0.000000F, 0.500000F},
+    {0.000000F, 0.000000F, 0.750000F},
+    {0.000000F, 0.000000F, 1.000000F},
+    {0.000000F, 0.250000F, 1.000000F},
+    {0.000000F, 0.500000F, 1.000000F},
+    {0.000000F, 0.750000F, 1.000000F},
+    {0.000000F, 1.000000F, 1.000000F},
+    {0.250000F, 1.000000F, 0.750000F},
+    {0.500000F, 1.000000F, 0.500000F},
+    {0.750000F, 1.000000F, 0.250000F},
+    {1.000000F, 1.000000F, 0.000000F},
+    {1.000000F, 0.750000F, 0.000000F},
+    {1.000000F, 0.500000F, 0.000000F},
+    {1.000000F, 0.250000F, 0.000000F},
+    {1.000000F, 0.000000F, 0.000000F},
+    {0.500000F, 0.000000F, 0.000000F},
+};
+
+const std::vector<Vector3<float>> ColorPalette::BlueCyanYellowRed256 = {
+    0x00007fU, 0x000083U, 0x000087U, 0x00008bU, 0x00008fU, 0x000093U, 0x000097U,
+    0x00009bU, 0x00009fU, 0x0000a3U, 0x0000a7U, 0x0000abU, 0x0000afU, 0x0000b3U,
+    0x0000b7U, 0x0000bbU, 0x0000bfU, 0x0000c3U, 0x0000c7U, 0x0000cbU, 0x0000cfU,
+    0x0000d3U, 0x0000d7U, 0x0000dbU, 0x0000dfU, 0x0000e3U, 0x0000e7U, 0x0000ebU,
+    0x0000efU, 0x0000f3U, 0x0000f7U, 0x0000fbU, 0x0000ffU, 0x0003ffU, 0x0007ffU,
+    0x000bffU, 0x000fffU, 0x0013ffU, 0x0017ffU, 0x001bffU, 0x001fffU, 0x0023ffU,
+    0x0027ffU, 0x002bffU, 0x002fffU, 0x0033ffU, 0x0037ffU, 0x003bffU, 0x003fffU,
+    0x0043ffU, 0x0047ffU, 0x004bffU, 0x004fffU, 0x0053ffU, 0x0057ffU, 0x005bffU,
+    0x005fffU, 0x0063ffU, 0x0067ffU, 0x006bffU, 0x006fffU, 0x0073ffU, 0x0077ffU,
+    0x007bffU, 0x007fffU, 0x0083ffU, 0x0087ffU, 0x008bffU, 0x008fffU, 0x0093ffU,
+    0x0097ffU, 0x009bffU, 0x009fffU, 0x00a3ffU, 0x00a7ffU, 0x00abffU, 0x00afffU,
+    0x00b3ffU, 0x00b7ffU, 0x00bbffU, 0x00bfffU, 0x00c3ffU, 0x00c7ffU, 0x00cbffU,
+    0x00cfffU, 0x00d3ffU, 0x00d7ffU, 0x00dbffU, 0x00dfffU, 0x00e3ffU, 0x00e7ffU,
+    0x00ebffU, 0x00efffU, 0x00f3ffU, 0x00f7ffU, 0x00fbffU, 0x00ffffU, 0x03fffbU,
+    0x07fff7U, 0x0bfff3U, 0x0fffefU, 0x13ffebU, 0x17ffe7U, 0x1bffe3U, 0x1fffdfU,
+    0x23ffdbU, 0x27ffd7U, 0x2bffd3U, 0x2fffcfU, 0x33ffcbU, 0x37ffc7U, 0x3bffc3U,
+    0x3fffbfU, 0x43ffbbU, 0x47ffb7U, 0x4bffb3U, 0x4fffafU, 0x53ffabU, 0x57ffa7U,
+    0x5bffa3U, 0x5fff9fU, 0x63ff9bU, 0x67ff97U, 0x6bff93U, 0x6fff8fU, 0x73ff8bU,
+    0x77ff87U, 0x7bff83U, 0x7fff7fU, 0x83ff7bU, 0x87ff77U, 0x8bff73U, 0x8fff6fU,
+    0x93ff6bU, 0x97ff67U, 0x9bff63U, 0x9fff5fU, 0xa3ff5bU, 0xa7ff57U, 0xabff53U,
+    0xafff4fU, 0xb3ff4bU, 0xb7ff47U, 0xbbff43U, 0xbfff3fU, 0xc3ff3bU, 0xc7ff37U,
+    0xcbff33U, 0xcfff2fU, 0xd3ff2bU, 0xd7ff27U, 0xdbff23U, 0xdfff1fU, 0xe3ff1bU,
+    0xe7ff17U, 0xebff13U, 0xefff0fU, 0xf3ff0bU, 0xf7ff07U, 0xfbff03U, 0xffff00U,
+    0xfffb00U, 0xfff700U, 0xfff300U, 0xffef00U, 0xffeb00U, 0xffe700U, 0xffe300U,
+    0xffdf00U, 0xffdb00U, 0xffd700U, 0xffd300U, 0xffcf00U, 0xffcb00U, 0xffc700U,
+    0xffc300U, 0xffbf00U, 0xffbb00U, 0xffb700U, 0xffb300U, 0xffaf00U, 0xffab00U,
+    0xffa700U, 0xffa300U, 0xff9f00U, 0xff9b00U, 0xff9700U, 0xff9300U, 0xff8f00U,
+    0xff8b00U, 0xff8700U, 0xff8300U, 0xff7f00U, 0xff7b00U, 0xff7700U, 0xff7300U,
+    0xff6f00U, 0xff6b00U, 0xff6700U, 0xff6300U, 0xff5f00U, 0xff5b00U, 0xff5700U,
+    0xff5300U, 0xff4f00U, 0xff4b00U, 0xff4700U, 0xff4300U, 0xff3f00U, 0xff3b00U,
+    0xff3700U, 0xff3300U, 0xff2f00U, 0xff2b00U, 0xff2700U, 0xff2300U, 0xff1f00U,
+    0xff1b00U, 0xff1700U, 0xff1300U, 0xff0f00U, 0xff0b00U, 0xff0700U, 0xff0300U,
+    0xff0000U, 0xfa0000U, 0xf60000U, 0xf20000U, 0xee0000U, 0xea0000U, 0xe60000U,
+    0xe20000U, 0xde0000U, 0xd90000U, 0xd50000U, 0xd10000U, 0xcd0000U, 0xc90000U,
+    0xc50000U, 0xc10000U, 0xbd0000U, 0xb90000U, 0xb40000U, 0xb00000U, 0xac0000U,
+    0xa80000U, 0xa40000U, 0xa00000U, 0x9c0000U, 0x980000U, 0x940000U, 0x8f0000U,
+    0x8b0000U, 0x870000U, 0x830000U, 0x7f0000U,
+};
+
+const std::vector<Vector3<float>> ColorPalette::BlueCyanGreenYellowRed16 = {
     {0.000000F, 0.000000F, 1.000000F},
     {0.000000F, 0.333333F, 1.000000F},
     {0.000000F, 0.666667F, 1.000000F},
@@ -163,32 +241,62 @@ const std::vector<Vector3<float>> ColorPalette::blueCyanGreenYellowRed16 = {
     {1.000000F, 0.000000F, 0.000000F},
 };
 
-std::vector<Vector3<float>> ColorPalette::blueGreenRed(size_t nColors)
+const std::vector<Vector3<float>> ColorPalette::WindowsXp32 = {
+    0x996600U, 0xcc9900U, 0xffcc00U, 0xffff00U, 0xffff99U, 0xffdb9dU,
+    0xffcc66U, 0xff9933U, 0xff794bU, 0xff3300U, 0x990000U, 0x333366U,
+    0x003399U, 0x0066ccU, 0x0083d7U, 0x0099ffU,
+
+    0x3e9adeU, 0x99ccffU, 0xb4e2ffU, 0xdeffffU, 0xffccffU, 0xccccffU,
+    0x9999ffU, 0x6666ccU, 0x9999ccU, 0x666699U, 0x006600U, 0x009900U,
+    0x66cc33U, 0x99ff66U, 0xccf4ccU, 0xffffffU};
+
+std::vector<Vector3<float>> ColorPalette::blueCyanYellowRed(size_t nColors)
 {
     std::vector<Vector3<float>> colormap;
     colormap.resize(nColors);
 
-    size_t n1 = nColors / 3;
-    float delta = 1.0F / static_cast<float>(n1);
-    for (size_t i = 0; i < n1; i++)
+    // Blue
+    size_t i1 = nColors / 8;
+    float delta = 0.5F / static_cast<float>(i1);
+    for (size_t i = 0; i < i1; i++)
     {
         float v = delta * static_cast<float>(i);
+        colormap[i].set(0.0F, 0.0F, 0.5F + v);
+    }
+
+    // Blue to Cyan
+    size_t i2 = i1 + nColors / 4;
+    delta = 1.0F / static_cast<float>(i2 - i1);
+    for (size_t i = i1; i < i2; i++)
+    {
+        float v = delta * static_cast<float>(i - i1);
         colormap[i].set(0.0F, v, 1.0F);
     }
 
-    size_t n2 = n1 * 2;
-    delta = 1.0F / static_cast<float>(n2 - n1);
-    for (size_t i = n1; i < n2; i++)
+    // Cyan to Yellow
+    size_t i3 = i2 + nColors / 4;
+    delta = 1.0F / static_cast<float>(i3 - i2);
+    for (size_t i = i2; i < i3; i++)
     {
-        float v = delta * static_cast<float>(i - n1);
+        float v = delta * static_cast<float>(i - i2);
         colormap[i].set(v, 1.0F, 1.0F - v);
     }
 
-    delta = 1.0F / static_cast<float>(nColors - n2 - 1);
-    for (size_t i = n2; i < nColors; i++)
+    // Yellow to Red
+    size_t i4 = i3 + nColors / 4;
+    delta = 1.0F / static_cast<float>(i4 - i3);
+    for (size_t i = i3; i < i4; i++)
     {
-        float v = delta * static_cast<float>(i - n2);
+        float v = delta * static_cast<float>(i - i3);
         colormap[i].set(1.0F, 1.0F - v, 0.0F);
+    }
+
+    // Red
+    delta = 0.5F / static_cast<float>(nColors - i4 - 1);
+    for (size_t i = i4; i < nColors; i++)
+    {
+        float v = delta * static_cast<float>(i - i4);
+        colormap[i].set(1.0F - v, 0.0F, 0.0F);
     }
 
     return colormap;
@@ -199,34 +307,38 @@ std::vector<Vector3<float>> ColorPalette::blueCyanGreenYellowRed(size_t nColors)
     std::vector<Vector3<float>> colormap;
     colormap.resize(nColors);
 
-    size_t n1 = nColors / 5;
-    float delta = 1.0F / static_cast<float>(n1);
-    for (size_t i = 0; i < n1; i++)
+    // Blue to Cyan
+    size_t i1 = nColors / 5;
+    float delta = 1.0F / static_cast<float>(i1);
+    for (size_t i = 0; i < i1; i++)
     {
         float v = delta * static_cast<float>(i);
         colormap[i].set(0.0F, v, 1.0F);
     }
 
-    size_t n2 = n1 * 2;
-    delta = 1.0F / static_cast<float>(n2 - n1);
-    for (size_t i = n1; i < n2; i++)
+    // Cyan to Green
+    size_t i2 = i1 + nColors / 5;
+    delta = 1.0F / static_cast<float>(i2 - i1);
+    for (size_t i = i1; i < i2; i++)
     {
-        float v = delta * static_cast<float>(i - n1);
+        float v = delta * static_cast<float>(i - i1);
         colormap[i].set(0.0F, 1.0F, 1.0F - v);
     }
 
-    size_t n3 = n1 * 3;
-    delta = 1.0F / static_cast<float>(n3 - n2);
-    for (size_t i = n2; i < n3; i++)
+    // Green to Yellow
+    size_t i3 = i2 + nColors / 5;
+    delta = 1.0F / static_cast<float>(i3 - i2);
+    for (size_t i = i2; i < i3; i++)
     {
-        float v = delta * static_cast<float>(i - n2);
+        float v = delta * static_cast<float>(i - i2);
         colormap[i].set(v, 1.0F, 0.0F);
     }
 
-    delta = 1.0F / static_cast<float>(nColors - n3 - 1);
-    for (size_t i = n3; i < nColors; i++)
+    // Yellow to Red
+    delta = 1.0F / static_cast<float>(nColors - i3 - 1);
+    for (size_t i = i3; i < nColors; i++)
     {
-        float v = delta * static_cast<float>(i - n3);
+        float v = delta * static_cast<float>(i - i3);
         colormap[i].set(1.0F, 1.0F - v, 0.0F);
     }
 
