8d12ee5fcc9abadcbff8be6233d87bd6cfb2ed40
Vaclav Bubnik <vbubnik@email.cz>
2022-05-24 19:36:17 +0200

    Add documentation

--- doc/doxyfile.mk
+++ doc/doxyfile.mk
@@ -80,7 +80,7 @@ PROJECT_NUMBER         = 1.0
 #
 # newflag
 
-PROJECT_LOGO           = ../modules/applications/3dforest/application/resources/3dforest-48.png
+PROJECT_LOGO           = ../src/apps/3dforest/resources/3dforest-48.png
 
 # The OUTPUT_DIRECTORY tag is used to specify the (relative or absolute) path
 # into which the generated documentation will be written. If a relative path is
@@ -821,9 +821,8 @@ WARN_LOGFILE           =
 # Note: If this tag is empty the current directory is searched.
 
 INPUT                  = ../doc/userguide \
-                         ../doc/developerguide \
-                         ../modules \
-                         ../examples
+                         ../doc/developerguide
+#                         ../src
 
 # This tag can be used to specify the character encoding of the source files
 # that doxygen parses. Internally doxygen uses the UTF-8 encoding. Doxygen uses
---
+++ doc/output/cheatsheet_build.js
@@ -0,0 +1,7 @@
+var cheatsheet_build =
+[
+    [ "Prerequisites", "cheatsheet_build.html#Prerequisites", null ],
+    [ "Variants", "cheatsheet_build.html#Variants", null ],
+    [ "Build Using \"Unix Makefiles\" on Linux", "cheatsheet_build_linux_make.html", null ],
+    [ "Build Using \"MinGW Makefiles\" on Windows", "cheatsheet_build_windows_make.html", null ]
+];
