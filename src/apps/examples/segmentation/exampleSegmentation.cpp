dc97d949fdb518a2f992b81b6db7a149b5681667
Vaclav Bubnik <vbubnik@email.cz>
2022-06-26 16:25:28 +0200

    Update

--- src/apps/examples/segmentation/exampleSegmentation.cpp
+++ src/apps/examples/segmentation/exampleSegmentation.cpp
@@ -21,6 +21,7 @@
 
 #include <Editor.hpp>
 #include <Error.hpp>
+#include <Log.hpp>
 #include <SegmentationThread.hpp>
 
 #define PATH "dataset.las"
@@ -30,13 +31,37 @@ static void exampleSegmentation()
     Editor editor;
     editor.open(PATH);
 
-    int voxelSize = 1;
+    int voxelSize = 10;
     int threshold = 50;
 
     SegmentationThread segmentationThread(&editor);
     segmentationThread.create();
     segmentationThread.start(voxelSize, threshold);
     segmentationThread.wait();
+
+    const Voxels &voxels = editor.voxels();
+    LOG("size <" << voxels.size() << ">");
+    for (size_t i = 0; i < voxels.size(); i++)
+    {
+        const Voxels::Voxel &voxel = voxels.at(i);
+        LOG("i <" << voxel.i << ">");
+    }
+}
+
+static void add(std::vector<LasFile::Point> *points,
+                uint32_t x,
+                uint32_t y,
+                uint32_t z)
+{
+    LasFile::Point pt;
+
+    std::memset(&pt, 0, sizeof(LasFile::Point));
+
+    pt.x = x;
+    pt.y = y;
+    pt.z = z;
+
+    points->push_back(pt);
 }
 
 static void createDataset()
@@ -44,20 +69,10 @@ static void createDataset()
     // Create a dataset.
     std::vector<LasFile::Point> points;
 
-    points.resize(3);
-    std::memset(points.data(), 0, sizeof(LasFile::Point) * points.size());
-
-    points[0].x = 0;
-    points[0].y = 0;
-    points[0].z = 0;
-
-    points[1].x = 1;
-    points[1].y = 0;
-    points[1].z = 0;
-
-    points[2].x = 2;
-    points[2].y = 0;
-    points[2].z = 0;
+    add(&points, 0, 0, 0);
+    add(&points, 1, 0, 0);
+    add(&points, 0, 1, 0);
+    add(&points, 1, 1, 0);
 
     LasFile::create(PATH, points, {1, 1, 1}, {0, 0, 0});
 
