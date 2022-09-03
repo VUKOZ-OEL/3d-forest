02c6c9bc714b0a34f92454043f6ba7fa0d2748b2
Vaclav Bubnik <vbubnik@email.cz>
2022-09-03 12:18:54 +0200

    Add height map plugin

---
+++ src/plugins/heightmap/HeightMapModifier.cpp
@@ -0,0 +1,225 @@
+/*
+    Copyright 2020 VUKOZ
+
+    This file is part of 3D Forest.
+
+    3D Forest is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    3D Forest is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with 3D Forest.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/** @file HeightMapModifier.cpp */
+
+#include <ColorPalette.hpp>
+#include <Editor.hpp>
+#include <HeightMapModifier.hpp>
+#include <MainWindow.hpp>
+#include <Time.hpp>
+
+#include <QCoreApplication>
+#include <QProgressDialog>
+#include <QString>
+#include <QWidget>
+
+#define PLUGIN_HEIGHT_MAP_NAME "Heightmap"
+#define PLUGIN_HEIGHT_MAP_COLORMAP_MATLAB_JET "Matlab Jet"
+#define PLUGIN_HEIGHT_MAP_COLORMAP_VTK "VTK"
+#define PLUGIN_HEIGHT_MAP_COLORMAP_GRAY "Gray"
+#define PLUGIN_HEIGHT_MAP_COLORMAP_WIN_XP "Windows XP"
+#define PLUGIN_HEIGHT_MAP_COLORMAP_DEFAULT PLUGIN_HEIGHT_MAP_COLORMAP_MATLAB_JET
+#define PLUGIN_HEIGHT_MAP_COLORS_DEFAULT 256
+
+HeightMapModifier::HeightMapModifier()
+    : mainWindow_(nullptr),
+      editor_(nullptr),
+      previewEnabled_(false)
+{
+}
+
+void HeightMapModifier::initialize(MainWindow *mainWindow)
+{
+    mainWindow_ = mainWindow;
+    editor_ = &mainWindow->editor();
+    colormap_ = createColormap(PLUGIN_HEIGHT_MAP_COLORMAP_DEFAULT,
+                               PLUGIN_HEIGHT_MAP_COLORS_DEFAULT);
+}
+
+void HeightMapModifier::setColormap(const QString &name, int colorCount)
+{
+    bool previewEnabled;
+    mutex_.lock();
+    previewEnabled = previewEnabled_;
+    colormap_ = createColormap(name, colorCount);
+    mutex_.unlock();
+
+    if (previewEnabled)
+    {
+        setPreviewEnabled(previewEnabled);
+    }
+}
+
+void HeightMapModifier::setPreviewEnabled(bool enabled,
+                                          bool update,
+                                          bool reload)
+{
+    if (update)
+    {
+        mainWindow_->suspendThreads();
+
+        mutex_.lock();
+        previewEnabled_ = enabled;
+        mutex_.unlock();
+
+        editor_->lock();
+        if (reload)
+        {
+            editor_->viewports().setState(Page::STATE_READ);
+        }
+        else
+        {
+            editor_->viewports().setState(Page::STATE_RUN_MODIFIERS);
+        }
+        editor_->unlock();
+
+        mainWindow_->resumeThreads();
+    }
+    else
+    {
+        mutex_.lock();
+        previewEnabled_ = enabled;
+        mutex_.unlock();
+    }
+}
+
+bool HeightMapModifier::isPreviewEnabled()
+{
+    bool ret;
+    mutex_.lock();
+    ret = previewEnabled_;
+    mutex_.unlock();
+    return ret;
+}
+
+void HeightMapModifier::applyModifier(Page *page)
+{
+    mutex_.lock();
+
+    double zMin = editor_->clipBoundary().min(2);
+    double zLen = editor_->clipBoundary().max(2) - zMin;
+    double colorDelta = 1.0 / static_cast<double>(colormap_.size() - 1);
+
+    double zLenInv = 0;
+    if (zLen > 0)
+    {
+        zLenInv = 1.0 / zLen;
+    }
+
+    const std::vector<uint32_t> &selection = page->selection;
+
+    for (size_t i = 0; i < page->selectionSize; i++)
+    {
+        size_t row = selection[i];
+
+        double z = page->position[3 * row + 2];
+        double zNorm = (z - zMin) * zLenInv;
+
+        size_t colorIndex = static_cast<size_t>(zNorm / colorDelta);
+
+        page->renderColor[row * 3 + 0] *= colormap_[colorIndex][0];
+        page->renderColor[row * 3 + 1] *= colormap_[colorIndex][1];
+        page->renderColor[row * 3 + 2] *= colormap_[colorIndex][2];
+    }
+
+    mutex_.unlock();
+}
+
+void HeightMapModifier::apply(QWidget *widget)
+{
+    mainWindow_->suspendThreads();
+
+    Query query(editor_);
+    query.selectBox(editor_->clipBoundary());
+    query.exec();
+
+    int maximum = static_cast<int>(query.pageSizeEstimate());
+
+    QProgressDialog progressDialog(widget);
+    progressDialog.setCancelButtonText(QObject::tr("&Cancel"));
+    progressDialog.setRange(0, maximum);
+    progressDialog.setWindowTitle(QObject::tr(PLUGIN_HEIGHT_MAP_NAME));
+    progressDialog.setWindowModality(Qt::WindowModal);
+    progressDialog.setMinimumDuration(0);
+    progressDialog.show();
+
+    for (int i = 0; i < maximum; i++)
+    {
+        // Update progress i
+        progressDialog.setValue(i + 1);
+        progressDialog.setLabelText(
+            QObject::tr("Processing %1 of %n...", nullptr, maximum).arg(i + 1));
+
+        QCoreApplication::processEvents();
+        if (progressDialog.wasCanceled())
+        {
+            break;
+        }
+
+        // Process step i
+        editor_->lock();
+        if (query.nextPage())
+        {
+            // editor_->applyFilters(query.page());
+            // editor_->flush(query.page());
+        }
+        editor_->unlock();
+    }
+    progressDialog.setValue(progressDialog.maximum());
+
+    mainWindow_->resumeThreads();
+}
+
+std::vector<Vector3<float>> HeightMapModifier::createColormap(
+    const QString &name,
+    int colorCount)
+{
+    size_t n = static_cast<size_t>(colorCount);
+
+    if (name == PLUGIN_HEIGHT_MAP_COLORMAP_MATLAB_JET)
+    {
+        return ColorPalette::blueCyanYellowRed(n);
+    }
+    else if (name == PLUGIN_HEIGHT_MAP_COLORMAP_VTK)
+    {
+        return ColorPalette::blueCyanGreenYellowRed(n);
+    }
+    else if (name == PLUGIN_HEIGHT_MAP_COLORMAP_GRAY)
+    {
+        return ColorPalette::gray(n);
+    }
+    else if (name == PLUGIN_HEIGHT_MAP_COLORMAP_WIN_XP)
+    {
+        return ColorPalette::WindowsXp32;
+    }
+    else
+    {
+        // White
+        std::vector<Vector3<float>> colormap;
+        colormap.resize(n);
+
+        for (size_t i = 0; i < n; i++)
+        {
+            colormap[i].set(1.0F, 1.0F, 1.0F);
+        }
+
+        return colormap;
+    }
+}
---
+++ src/plugins/heightmap/HeightMapWindow.cpp
@@ -0,0 +1,165 @@
+/*
+    Copyright 2020 VUKOZ
+
+    This file is part of 3D Forest.
+
+    3D Forest is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    3D Forest is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with 3D Forest.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/** @file HeightMapWindow.cpp */
+
+#include <HeightMapModifier.hpp>
+#include <HeightMapWindow.hpp>
+#include <MainWindow.hpp>
+
+#include <QCheckBox>
+#include <QCloseEvent>
+#include <QComboBox>
+#include <QCoreApplication>
+#include <QDebug>
+#include <QGridLayout>
+#include <QGroupBox>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QMainWindow>
+#include <QProgressDialog>
+#include <QPushButton>
+#include <QSpinBox>
+#include <QVBoxLayout>
+
+#define PLUGIN_HEIGHT_MAP_NAME "Height Map"
+#define PLUGIN_HEIGHT_MAP_COLORMAP_MATLAB_JET "Matlab Jet"
+#define PLUGIN_HEIGHT_MAP_COLORMAP_VTK "VTK"
+#define PLUGIN_HEIGHT_MAP_COLORMAP_GRAY "Gray"
+#define PLUGIN_HEIGHT_MAP_COLORMAP_WIN_XP "Windows XP"
+#define PLUGIN_HEIGHT_MAP_COLORMAP_DEFAULT PLUGIN_HEIGHT_MAP_COLORMAP_MATLAB_JET
+#define PLUGIN_HEIGHT_MAP_COLORS_MAX 65536
+#define PLUGIN_HEIGHT_MAP_COLORS_DEFAULT 256
+
+HeightMapWindow::HeightMapWindow(MainWindow *mainWindow,
+                                 HeightMapModifier *modifier)
+    : QDockWidget(mainWindow),
+      mainWindow_(mainWindow),
+      modifier_(modifier)
+{
+    // Widgets colormap
+    colorCountSpinBox_ = new QSpinBox;
+    colorCountSpinBox_->setRange(1, PLUGIN_HEIGHT_MAP_COLORS_MAX);
+    colorCountSpinBox_->setValue(PLUGIN_HEIGHT_MAP_COLORS_DEFAULT);
+    colorCountSpinBox_->setSingleStep(1);
+
+    connect(colorCountSpinBox_,
+            SIGNAL(valueChanged(int)),
+            this,
+            SLOT(colorCountChanged(int)));
+
+    colormapComboBox_ = new QComboBox;
+    colormapComboBox_->addItem(PLUGIN_HEIGHT_MAP_COLORMAP_MATLAB_JET);
+    colormapComboBox_->addItem(PLUGIN_HEIGHT_MAP_COLORMAP_VTK);
+    colormapComboBox_->addItem(PLUGIN_HEIGHT_MAP_COLORMAP_GRAY);
+    colormapComboBox_->addItem(PLUGIN_HEIGHT_MAP_COLORMAP_WIN_XP);
+    colormapComboBox_->setCurrentText(PLUGIN_HEIGHT_MAP_COLORMAP_DEFAULT);
+
+    connect(colormapComboBox_,
+            &QComboBox::activated,
+            this,
+            &HeightMapWindow::colormapChanged);
+
+    // Widgets apply
+    // connect() with '&' has less features than classic SIGNAL()/SLOT()
+    // in the current Qt version but it is easier to write.
+    previewCheckBox_ = new QCheckBox;
+    connect(previewCheckBox_,
+            &QCheckBox::stateChanged,
+            this,
+            &HeightMapWindow::previewChanged);
+
+    applyButton_ = new QPushButton(tr("Apply and save"));
+    applyButton_->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+    applyButton_->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
+    applyButton_->setDisabled(true);
+    connect(applyButton_,
+            &QAbstractButton::clicked,
+            this,
+            &HeightMapWindow::apply);
+
+    // Layout
+    QGridLayout *groupBoxLayout = new QGridLayout;
+    groupBoxLayout->addWidget(new QLabel(tr("N colors")), 0, 0);
+    groupBoxLayout->addWidget(colorCountSpinBox_, 0, 1);
+    groupBoxLayout->addWidget(new QLabel(tr("Colormap")), 1, 0);
+    groupBoxLayout->addWidget(colormapComboBox_, 1, 1);
+    groupBoxLayout->setColumnStretch(1, 1);
+
+    QHBoxLayout *hbox = new QHBoxLayout;
+    hbox->addWidget(previewCheckBox_);
+    hbox->addWidget(new QLabel(tr("Preview")));
+    hbox->addStretch();
+    hbox->addWidget(applyButton_, 0, Qt::AlignRight);
+
+    QVBoxLayout *vbox = new QVBoxLayout;
+    vbox->addLayout(groupBoxLayout);
+    vbox->addSpacing(10);
+    vbox->addLayout(hbox);
+    vbox->addStretch();
+
+    // Dock
+    widget_ = new QWidget;
+    widget_->setLayout(vbox);
+
+    setWidget(widget_);
+    setWindowTitle(QObject::tr(PLUGIN_HEIGHT_MAP_NAME));
+    setFloating(true);
+    setAllowedAreas(Qt::LeftDockWidgetArea | Qt::RightDockWidgetArea);
+    mainWindow_->addDockWidget(Qt::RightDockWidgetArea, this);
+}
+
+void HeightMapWindow::colorCountChanged(int i)
+{
+    (void)i;
+    modifier_->setColormap(colormapComboBox_->currentText(),
+                           colorCountSpinBox_->value());
+}
+
+void HeightMapWindow::colormapChanged(int index)
+{
+    (void)index;
+    modifier_->setColormap(colormapComboBox_->currentText(),
+                           colorCountSpinBox_->value());
+}
+
+void HeightMapWindow::previewChanged(int index)
+{
+    (void)index;
+    modifier_->setPreviewEnabled(previewCheckBox_->isChecked());
+}
+
+void HeightMapWindow::apply()
+{
+    // Filter is active during proccesing
+    modifier_->setPreviewEnabled(true, false);
+    modifier_->apply(mainWindow_);
+    modifier_->setPreviewEnabled(previewCheckBox_->isChecked(), true, true);
+}
+
+void HeightMapWindow::closeEvent(QCloseEvent *event)
+{
+    if (previewCheckBox_->isChecked())
+    {
+        previewCheckBox_->setChecked(false);
+        modifier_->setPreviewEnabled(false);
+    }
+
+    event->accept();
+}
