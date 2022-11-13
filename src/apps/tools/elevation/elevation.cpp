0a0438cdbfb822950399153c619ec054377ff5b9
Vaclav Bubnik <vbubnik@email.cz>
2022-11-13 19:48:15 +0100

    Use argument parser

--- src/apps/tools/elevation/elevation.cpp
+++ src/apps/tools/elevation/elevation.cpp
@@ -19,14 +19,15 @@
 
 /** @file elevation.cpp */
 
+#include <ArgumentParser.hpp>
 #include <Editor.hpp>
 #include <Elevation.hpp>
 #include <Error.hpp>
 #include <Log.hpp>
 
-static void elevation(const char *inputPath,
-                      size_t pointsPerCell,
-                      double cellLengthMinPercent)
+static void elevationCompute(const std::string inputPath,
+                             size_t pointsPerCell,
+                             double cellLengthMinPercent)
 {
     // Open input file in editor.
     Editor editor;
@@ -46,38 +47,84 @@ static void elevation(const char *inputPath,
     std::cout << "elevation maximum <" << e.maximum() << ">" << std::endl;
 }
 
+static void elevationPrint(const std::string inputPath)
+{
+    Editor editor;
+    editor.open(inputPath);
+
+    uint64_t nPoints = 0;
+    double elevationMinimum = 0;
+    double elevationMaximum = 0;
+
+    Query query(&editor);
+    query.selectBox(editor.clipBoundary());
+    query.exec();
+    while (query.next())
+    {
+        double elevation = query.elevation();
+
+        if (nPoints == 0)
+        {
+            elevationMinimum = elevation;
+            elevationMaximum = elevation;
+        }
+        else
+        {
+            updateRange(elevation, elevationMinimum, elevationMaximum);
+        }
+
+        nPoints++;
+    }
+
+    std::cout << "number of points  : " << nPoints << std::endl;
+    std::cout << "minimum elevation : " << elevationMinimum << std::endl;
+    std::cout << "maximum elevation : " << elevationMaximum << std::endl;
+}
+
+static void elevationPlot(const std::string inputPath,
+                          size_t plotSize,
+                          double elevationMinimum,
+                          double elevationMaximum)
+{
+}
+
 int main(int argc, char *argv[])
 {
-    const char *inputPath = nullptr;
-    size_t pointsPerCell = 10000;
-    double cellLengthMinPercent = 5.;
-
-    if (argc > 1)
-    {
-        inputPath = argv[1];
-    }
-
-    if (argc > 2)
-    {
-        int v = atoi(argv[2]);
-        if (v > 0)
-        {
-            pointsPerCell = static_cast<size_t>(v);
-        }
-    }
-
-    if (argc > 3)
-    {
-        double v = atof(argv[3]);
-        if (v > 0.)
-        {
-            cellLengthMinPercent = v;
-        }
-    }
+    ArgumentParser arg;
+    arg.add("--input", "");
+    arg.add("--compute", "");
+    arg.add("--cell-points", "10000");
+    arg.add("--cell-size-min", "5");
+    arg.add("--print", "");
+    arg.add("--plot", "");
+    arg.add("--plot-size", "100");
+    arg.add("--plot-min", "0");
+    arg.add("--plot-max", "0");
+    arg.parse(argc, argv);
 
     try
     {
-        elevation(inputPath, pointsPerCell, cellLengthMinPercent);
+        if (arg.contains("--compute"))
+        {
+            elevationCompute(arg.toString("--input"),
+                             arg.toSize("--cell-points"),
+                             arg.toDouble("--cell-size-min"));
+        }
+        else if (arg.contains("--print"))
+        {
+            elevationPrint(arg.toString("--input"));
+        }
+        else if (arg.contains("--plot"))
+        {
+            elevationPlot(arg.toString("--input"),
+                          arg.toSize("--plot-size"),
+                          arg.toDouble("--plot-min"),
+                          arg.toDouble("--plot-max"));
+        }
+        else
+        {
+            THROW("Unknown action");
+        }
     }
     catch (std::exception &e)
     {
