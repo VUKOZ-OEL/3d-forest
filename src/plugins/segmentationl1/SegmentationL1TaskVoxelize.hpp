63b724d9f74b3c10376989d87151adc02b5d5121
Vaclav Bubnik <vbubnik@email.cz>
2023-02-19 15:08:56 +0100

    Add voxelize task.

---
+++ src/plugins/segmentationl1/SegmentationL1TaskVoxelize.hpp
@@ -0,0 +1,40 @@
+/*
+    Copyright 2020 VUKOZ
+
+    This file is part of 3D Forest.
+
+    3D Forest is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    3D Forest is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with 3D Forest.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/** @file SegmentationL1TaskVoxelize.hpp */
+
+#ifndef SEGMENTATION_L1_TASK_VOXELIZE_HPP
+#define SEGMENTATION_L1_TASK_VOXELIZE_HPP
+
+#include <SegmentationL1TaskInterface.hpp>
+
+/** Segmentation L1 Task Voxelize. */
+class SegmentationL1TaskVoxelize : public SegmentationL1TaskInterface
+{
+public:
+    virtual void initialize(SegmentationL1Context *context);
+    virtual void next();
+
+private:
+    SegmentationL1Context *context_;
+
+    void step();
+};
+
+#endif /* SEGMENTATION_L1_TASK_VOXELIZE_HPP */
