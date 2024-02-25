5434b733f437d4d01226cdc2202753aa180a5ea2
Vaclav Bubnik <vbubnik@email.cz>
2024-02-25 20:38:04 +0100

    Add help dialog for file import.

--- src/plugins/importfile/ImportFileDialog.cpp
+++ src/plugins/importfile/ImportFileDialog.cpp
@@ -21,6 +21,7 @@
 
 // Include 3D Forest.
 #include <ImportFileDialog.hpp>
+#include <InfoDialog.hpp>
 #include <MainWindow.hpp>
 #include <ThemeIcon.hpp>
 
@@ -39,7 +40,9 @@
 
 #define ICON(name) (ThemeIcon(":/importfile/", name))
 
-ImportFileDialog::ImportFileDialog(MainWindow *mainWindow) : QDialog(mainWindow)
+ImportFileDialog::ImportFileDialog(MainWindow *mainWindow)
+    : QDialog(mainWindow),
+      helpDialog_(nullptr)
 {
     // Widgets.
     QLabel *description = new QLabel(tr("Import action will modify "
@@ -68,7 +71,12 @@ ImportFileDialog::ImportFileDialog(MainWindow *mainWindow) : QDialog(mainWindow)
     copyExtraBytesCheckBox_->setChecked(true);
 
     // Dialog buttons.
+    helpButton_ = new QPushButton(tr("Help"));
+    helpButton_->setIcon(THEME_ICON("question"));
+    connect(helpButton_, SIGNAL(clicked()), this, SLOT(slotHelp()));
+
     acceptButton_ = new QPushButton(tr("Import"));
+    acceptButton_->setIcon(THEME_ICON("run"));
     connect(acceptButton_, SIGNAL(clicked()), this, SLOT(slotAccept()));
 
     rejectButton_ = new QPushButton(tr("Cancel"));
@@ -92,6 +100,7 @@ ImportFileDialog::ImportFileDialog(MainWindow *mainWindow) : QDialog(mainWindow)
     options->setLayout(optionsLayout);
 
     QHBoxLayout *dialogButtons = new QHBoxLayout;
+    dialogButtons->addWidget(helpButton_);
     dialogButtons->addStretch();
     dialogButtons->addWidget(acceptButton_);
     dialogButtons->addWidget(rejectButton_);
@@ -135,4 +144,49 @@ SettingsImport ImportFileDialog::settings() const
     settingsImport.copyExtraBytes = copyExtraBytesCheckBox_->isChecked();
 
     return settingsImport;
+}
+
+void ImportFileDialog::slotHelp()
+{
+    QString t;
+    t = "<h3>Import File</h3>"
+        "<h2>Options</h2>"
+        "The import file dialog allows to set the following options: "
+        "<ul>"
+        "<li><b>Convert to v1.4+</b> -"
+        " Convert LAS file to version 1.4 if it is in lower version."
+        " Version 1.4 allows to use more classifications, GPS coordinates,"
+        " etc.</li>"
+        "<li><b>Center on screen</b> -"
+        " Center imported data set in the middle of screen.</li>"
+        "<li><b>Randomize points</b> -"
+        " It is suggested to randomize the order of points in LAS files"
+        " to prevent eye popping artifacts caused displaying subsets"
+        " of points by level of details. It is possible to uncheck this "
+        " option if a file was already randomized.</li>"
+        "<li><b>Copy extra bytes</b> -"
+        " If this option is checked, then the import process preservers"
+        " all extra bytes in each point which are stored beyond the size"
+        " specified by LAS specification.</li>"
+        "</ul>"
+        "<h2>Modifications of the Original Input File</h2>"
+        "<ol>"
+        "<li>The points will be sorted by 3D spatial index"
+        " for fast access.</li>"
+        "<li>Some point data values will be normalized "
+        " (fixed) to match LAS specification.</li>"
+        "<li>Nonstandard application specific extra bytes "
+        " can be optionally removed from point data.</li>"
+        "</ol>";
+
+    if (!helpDialog_)
+    {
+        helpDialog_ = new InfoDialog(this, 550, 450);
+        helpDialog_->setWindowTitle(tr("Import File Help"));
+        helpDialog_->setText(t);
+    }
+
+    helpDialog_->show();
+    helpDialog_->raise();
+    helpDialog_->activateWindow();
 }
