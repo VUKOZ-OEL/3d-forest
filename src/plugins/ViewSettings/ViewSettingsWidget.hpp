796bcc892a0281dbde43d7284eda4459ee5a36a6
Vaclav Bubnik <vbubnik@email.cz>
2025-03-15 20:23:46 +0100

    Decrease view settings widget height.

--- src/plugins/ViewSettings/ViewSettingsWidget.hpp
+++ src/plugins/ViewSettings/ViewSettingsWidget.hpp
@@ -45,7 +45,7 @@ class ViewSettingsWidget : public QWidget
 public:
     ViewSettingsWidget(MainWindow *mainWindow);
 
-    QSize sizeHint() const override { return QSize(300, 240); }
+    QSize sizeHint() const override { return QSize(300, 200); }
 
 public slots:
     void slotUpdate(void *sender, const QSet<Editor::Type> &target);
