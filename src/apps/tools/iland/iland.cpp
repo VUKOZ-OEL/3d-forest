719ac7f502dab9a9946678cb9ae8e7b3c9461677
Vaclav Bubnik <vbubnik@email.cz>
2025-09-21 19:13:41 +0200

    Run iLand model from the command line.

--- src/apps/tools/iland/iland.cpp
+++ src/apps/tools/iland/iland.cpp
@@ -22,18 +22,100 @@
 // Include std.
 #include <iostream>
 
+// Include 3D Forest.
+#include <ArgumentParser.hpp>
+#include <Error.hpp>
+
 // Include Qt.
+#include <QCoreApplication>
 #include <QString>
 
 // Include 3rd party.
+#include "global.h"
+#include "model.h"
+#include "modelcontroller.h"
 #include <version.h>
+
+// Include local.
+#define LOG_MODULE_NAME "iland"
+#include <Log.hpp>
+
+static std::string toStdString(const QString &str)
+{
+    return str.toUtf8().constData();
+}
+
+static void iLandModelRun(const QString &xmlName, int years)
+{
+    ModelController iLandModel;
+    GlobalSettings::instance()->setModelController(&iLandModel);
+
+    iLandModel.setFileName(xmlName);
+    if (iLandModel.hasError())
+    {
+        THROW("setFileName: " + toStdString(iLandModel.lastError()));
+    }
+
+    iLandModel.create();
+    if (iLandModel.hasError())
+    {
+        THROW("create: " + toStdString(iLandModel.lastError()));
+    }
+
+    iLandModel.run(years);
+    if (iLandModel.hasError())
+    {
+        THROW("run: " + toStdString(iLandModel.lastError()));
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    (void)argc;
-    (void)argv;
+    int rc = 1;
 
-    std::cout << compiler().toUtf8().constData() << std::endl;
+    LOGGER_START_FILE("log_iland.txt");
 
-    return 0;
+    try
+    {
+        QCoreApplication app(argc, argv);
+
+        ArgumentParser arg("executes iland-model");
+
+        arg.add("-f",
+                "--file",
+                "",
+                "Path to the input file to be processed.",
+                true);
+
+        arg.add("-y",
+                "--years",
+                "1",
+                "The number of years to run the simulation");
+
+        if (arg.parse(argc, argv))
+        {
+            QString xmlName = QString::fromStdString(arg.toString("--file"));
+            int years = arg.toInt("--years");
+
+            iLandModelRun(xmlName, years);
+        }
+
+        rc = 0;
+    }
+    catch (const IException &e)
+    {
+        std::cerr << "error: " << toStdString(e.message()) << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "error: " << e.what() << std::endl;
+    }
+    catch (...)
+    {
+        std::cerr << "error: unknown" << std::endl;
+    }
+
+    LOGGER_STOP_FILE;
+
+    return rc;
 }
